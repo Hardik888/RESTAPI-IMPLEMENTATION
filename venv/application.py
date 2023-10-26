8f2b531cb3b41cca157c2c146ba522ccf504235a
Hardik888 <hardik89990@gmail.com>
2023-10-26 20:34:02 +0530

    Main code

---
+++ venv/application.py
@@ -0,0 +1,48 @@
+from flask import Flask,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+
+class Drinks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+
+@app.route('/')
+def home():
+    return "hello flask "
+   
+
+@app.route('/drinks')
+def get_drinks():
+    drinks =  Drinks.query.all()
+    
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name,'description':drink.
+            description}
+        output.append(drink_data)
+    
+    return {"drinks": output}
+  
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drinks.query.get_or_404(id)
+    return {"name": drink.name, "description": drink.description}
+
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drinks(name=request.json['name'],
+                   description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
